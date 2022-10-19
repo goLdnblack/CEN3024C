91c0f8eba61f497caafcb7e5ff0dfcd80ae531b5
Jesus Sanchez-Rivera <goLdnblack@users.noreply.github.com>
2022-10-18 22:09:50 -0400

    Updated UnitTest.java

--- SDLC_UnitTest/UnitTest.java
+++ SDLC_UnitTest/UnitTest.java
@@ -1,16 +1,24 @@
+import org.junit.Test;
 public class UnitTest
 {
 	// Generate filepath to different testcases
 	static String filepath = System.getProperty("user.dir");
 
+	@Test(expected = Exception.class)
+
 	public static void  main(String[] args)
 	{
+
 		TestCase01.test();
 		TestCase02.test();
 		TestCase03.test();
 		TestCase04.test();
 		
 		System.out.println("Sucess!");
+
+
+		
+
 	}
 }
 		
