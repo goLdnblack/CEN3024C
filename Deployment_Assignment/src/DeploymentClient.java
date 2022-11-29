29a9f21e95ccdfeca339e9214fb32e5dba056c24
Jesus Sanchez-Rivera <goLdnblack@users.noreply.github.com>
2022-11-28 22:42:05 -0500

    Client/host deployment with Javafx

---
+++ Deployment_Assignment/src/DeploymentClient.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.net.*;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class DeploymentClient extends Application
+{
+	DataOutputStream sendData = null;
+	DataInputStream receiveData = null;
+
+	public static void main(String[] args) throws Exception
+	{
+		//AppClient();
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception 
+	{
+		InetAddress host = InetAddress.getLocalHost();
+
+		BorderPane pane = new BorderPane();
+		pane.setPadding(new Insets(5, 5, 5, 5));
+		pane.setStyle("-fx-border-color: blue");
+		pane.setLeft(new Label("Enter number: "));
+
+		TextField tf = new TextField();
+		tf.setAlignment(Pos.BOTTOM_RIGHT);
+		pane.setCenter(tf);
+
+		BorderPane mainPane = new BorderPane();
+		TextArea ta = new TextArea();
+		mainPane.setCenter(new ScrollPane(ta));
+		mainPane.setTop(pane);
+
+		Scene scene = new Scene(mainPane, 450, 200);
+		primaryStage.setTitle("Client Application");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		tf.setOnAction(e ->
+		{
+			try 
+			{
+				//double r = Double.parseDouble(tf.getText().trim());
+				int num = Integer.parseInt(tf.getText().trim());
+
+				sendData.writeInt(num);
+				sendData.flush();
+
+				String isPrime = receiveData.readUTF();
+
+				ta.appendText(isPrime);
+			}
+			catch (IOException err)
+			{
+				System.err.println(err);
+			}
+		});
+
+		try
+		{
+			Socket socket = new Socket(host.getHostName(), 9567);
+			receiveData = new DataInputStream(socket.getInputStream());
+
+			sendData = new DataOutputStream(socket.getOutputStream());
+
+		}
+		catch (IOException e)
+		{
+			ta.appendText(e.toString() + '\n');
+		}
+	}	
+}
---
+++ Deployment_Assignment/src/DeploymentHost.java
@@ -0,0 +1,109 @@
+import java.io.*;
+import java.net.*;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+public class DeploymentHost extends Application
+{
+    // port to listen
+    private static int port = 9567;
+
+    public static void main(String[] args) throws Exception
+    {
+        //AppServer();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception 
+    {
+        TextArea ta = new TextArea();
+
+        Scene scene = new Scene(new ScrollPane(ta), 450, 200);
+        primaryStage.setTitle("Server application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        new Thread(() ->
+        {
+            try
+            {
+                ServerSocket serverSocket = new ServerSocket(port);
+                Platform.runLater(() ->
+                    ta.appendText("Server running.\n"));
+                    
+                Socket socket = serverSocket.accept();
+
+                DataInputStream clientInput = new DataInputStream(socket.getInputStream());
+                DataOutputStream serverOutput = new DataOutputStream(socket.getOutputStream());
+
+                while (true)
+                {
+                    /* 
+                    double r = clientInput.readDouble();
+
+                    double area = r * r * Math.PI;
+
+                    serverOutput.writeDouble(area);
+
+                    Platform.runLater( () ->
+                    {
+                        ta.appendText("Radius from client: " + r + "\n");
+                        ta.appendText("Area is " + String.format("%.2f", area) + "\n");
+                    });
+                    */
+
+                    int num = clientInput.readInt();
+
+                    boolean primeFlag = false;
+                    int even = 2;
+
+                    while (even <= num / 2)
+                    {
+                        if (num % even == 0)
+                        {
+                            primeFlag = true;
+                            break;
+                        }
+
+                        ++even;
+                    }
+
+                    /* 
+                    for (int x = 2; x <= num; ++x)
+                    {
+                        if (num % x == 0)
+                        {
+                            primeNum = true;
+                            break;
+                        }
+                    }
+                    */
+
+                    if (!primeFlag)
+                    {
+                        serverOutput.writeUTF("The number " + num + " is a prime number.\n");
+                    }
+                    else
+                    {
+                        serverOutput.writeUTF("The number " + num + " is not a prime number.\n");
+                    }
+
+                    Platform.runLater( () ->
+                    {
+                        ta.appendText("Number from client " + num + "\n");
+                    });
+
+                }
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
